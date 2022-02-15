c08ea7026380a6b0cf519c817e08daec23e18ede
KiraButter <oliviyazilber@mail.ru>
2022-02-15 21:00:23 +0300

    ...

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar( backgroundColor: Colors.purple ,
+            title:
+            const Text('Начало',
+                style: TextStyle(color: Colors.white))),
+        body:  Align(alignment: Alignment(0, 0), child: Text('Хотела вставить картинку, но не разобралась, как :<',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.normal, color: Colors.purple)))
+
+
+    ),
+  ));
+}
