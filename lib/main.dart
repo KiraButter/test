bd29e40a4cc93b2abf1df0d6cfa9ba79b8d470d0
Kira <oliviyazilber@mail.ru>
2022-02-17 16:21:09 +0300

    4

--- lib/main.dart
+++ lib/main.dart
@@ -7,29 +7,8 @@ void main() {
               backgroundColor: Color.fromARGB(255, 167, 14, 98),
               title:
                   const Text('Начало', style: TextStyle(color: Colors.white))),
-          body: Container(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget> [
-                Icon(
-                  Icons.adjust,
-                  size: 50,
-                  color: Colors.deepPurple,
-                ),
-                Icon(
-                  Icons.adjust,
-                  size: 30,
-                  color: Colors.deepOrange,
-                ),
-                Icon(
-                  Icons.adjust,
-                  size: 70,
-                )
-              ],
-            ),
-            decoration: BoxDecoration(
-                color: Color.fromARGB(146, 151, 235, 230),
-                borderRadius: BorderRadiusDirectional.circular(5)),
-          ))));
+          body: 
+          Image(image: AssetImage('pp/img/cat.jpg'),
+          
+  ))));
 }
