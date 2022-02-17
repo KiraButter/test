6d549afa5b4b3d1a8a9fbbdebfe5abf124f69c04
Kira <oliviyazilber@mail.ru>
2022-02-17 15:41:10 +0300

    2

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar( backgroundColor: Color.fromARGB(255, 167, 14, 98),
-            title:
-            const Text('Начало',
-                style: TextStyle(color: Colors.white))),
-       
-        body:  Container(
-          decoration: BoxDecoration(color: Color.fromARGB(146, 151, 235, 230), border: Border.all(width: 5, color: Colors.red), borderRadius: BorderRadiusDirectional.circular(5)),
-          alignment: Alignment.center,
-          child: Text(
-            'Container',
-            style: TextStyle(color: Colors.black, fontSize: 30),
+      home: Scaffold(
+          appBar: AppBar(
+              backgroundColor: Color.fromARGB(255, 167, 14, 98),
+              title:
+                  const Text('Начало', style: TextStyle(color: Colors.white))),
+          body: Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget> [
+                Icon(
+                  Icons.adjust,
+                  size: 50,
+                  color: Colors.deepPurple,
+                ),
+                Icon(
+                  Icons.adjust,
+                  size: 30,
+                  color: Colors.deepOrange,
+                ),
+                Icon(
+                  Icons.adjust,
+                  size: 70,
+                )
+              ],
             ),
-            )) 
-  ));
+            decoration: BoxDecoration(
+                color: Color.fromARGB(146, 151, 235, 230),
+                borderRadius: BorderRadiusDirectional.circular(5)),
+          ))));
 }
