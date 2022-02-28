85121fb66b26b3add5bc2356c30c8a51cf5e3533
Kira <oliviyazilber@mail.ru>
2022-02-28 20:26:49 +0300

    кнопки с текстом

--- lib/main.dart
+++ lib/main.dart
@@ -11,109 +11,92 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-              backgroundColor: const Color.fromARGB(255, 167, 14, 98),
-              centerTitle: true,
-              title: const Text('Трекер настроения',
-                  style: TextStyle(color: Colors.white)),
-              leading: const Icon(Icons.menu)),
-          body: Wrap(
-            alignment: WrapAlignment.center,
-            children: [
-              const Image(
-                image: AssetImage('pp/img/mood.png'),
-              ),
-              Column(children: [
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                    child: Text(
-                  'Сегодняшняя дата',
-                  style: TextStyle(
-                      color: Colors.deepPurple,
-                      fontFamily: 'Comic',
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                )),
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                    child: Text(
-                  'Выбери свое настроение по шкалеа от кота Сима',
-                  style: TextStyle(
-                      color: Colors.deepPurple,
-                      fontFamily: 'Comic',
-                      fontSize: 14),
-                )),
-              ]),
-              classButtons()
-            ],
-          )));
+        home: Scaffold(
+            appBar: AppBar(
+                backgroundColor: const Color.fromARGB(255, 167, 14, 98),
+                centerTitle: true,
+                title: const Text('Трекер настроения',
+                    style: TextStyle(color: Colors.white)),
+                leading: const Icon(Icons.menu)),
+            body: Wrap(
+              alignment: WrapAlignment.center,
+              children: [
+                const Image(
+                  image: AssetImage('pp/img/mood.png'),
+                ),
+                Column(children: [
+                  Container(
+                      padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                      child: Text(
+                        'Сегодняшняя дата',
+                        style: TextStyle(
+                            color: Colors.deepPurple,
+                            fontFamily: 'Comic',
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  Container(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                      child: Text(
+                        'Выбери свое настроение по шкалеа от кота Сима',
+                        style: TextStyle(
+                            color: Colors.deepPurple,
+                            fontFamily: 'Comic',
+                            fontSize: 14),
+                      )),
+                ]),
+                ButtonBar(
+                  alignment: MainAxisAlignment.center,
+                  children: [
+                    Wrap(
+                      runSpacing: 10,
+                      spacing: 5,
+                      children: [mood(), mood(), mood(), mood()],
+                    )
+                  ],
+                )
+              ],
+            )));
   }
-  
 }
 
-class classButtons extends StatelessWidget {
+class mood extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => checkedMood();
+}
+
+class checkedMood extends State<mood> {
+  Color backColor = Colors.transparent;
+  bool isVisibleCircle = false;
+
   @override
   Widget build(BuildContext context) {
-    return ButtonBar(
-                alignment: MainAxisAlignment.center,
-                children: [
-                  Wrap(
-                    runSpacing: 10,
-                    spacing: 5,
-                    children: [
-                      RaisedButton(
-                        child: Image(
-                          image: AssetImage('pp/img/mood1.png'),
-                          width: 150,
-                        ),
-                        color: Colors.white,
-                        onPressed: () {},
-                      ),
-                      RaisedButton(
-                        child: const Image(
-                          image: AssetImage('pp/img/mood2.png'),
-                          width: 150,
-                        ),
-                        color: Colors.white,
-                        onPressed: () {},
-                      ),
-                      RaisedButton(
-                        child: Image(
-                          image: AssetImage('pp/img/mood3.png'),
-                          width: 150,
-                        ),
-                        color: Colors.white,
-                        onPressed: () {},
-                      ),
-                      RaisedButton(
-                        child: Image(
-                          image: AssetImage('pp/img/mood4.png'),
-                          width: 150,
-                        ),
-                        color: Colors.white,
-                        onPressed: () {},
-                      ),
-                      RaisedButton(
-                        child: Image(
-                          image: AssetImage('pp/img/mood5.png'),
-                          width: 150,
-                        ),
-                        color: Colors.white,
-                        onPressed: () {},
-                      ),
-                      RaisedButton(
-                        child: Image(
-                          image: AssetImage('pp/img/mood6.png'),
-                          width: 150,
-                        ),
-                        color: Colors.white,
-                        onPressed: () {},
-                      )
-                    ],
-                  )
-                ],
-              );
+    return Column(
+      children: [
+        if (isVisibleCircle) new textMood(),
+        TextButton(
+        style:
+            ButtonStyle(backgroundColor: MaterialStateProperty.all(backColor)),
+        child: Image(
+          image: AssetImage('pp/img/mood1.png'),
+          width: 150,
+        ),
+        onPressed: () {
+          setState(() {
+            backColor = Color.fromARGB(123, 88, 14, 122);
+            isVisibleCircle = true;
+          });
+        })
+      ],
+    ) ;
   }
+}
 
+
+class textMood extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Текст под настр');
+  }
+  
 }
