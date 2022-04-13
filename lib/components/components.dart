08f1fec0c3fe73f47dce628035764c87c480a6c8
Kira <oliviyazilber@mail.ru>
2022-04-13 20:09:47 +0300

    список рецептов

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -3,3 +3,5 @@ export 'card1.dart';
 export 'card2.dart';
 export 'card3.dart';
 export 'circle_image.dart';
+export 'recipe_thumbnail.dart';
+export 'recipes_grid_view.dart';
--- lib/recipes_screen.dart
+++ lib/recipes_screen.dart
@@ -18,8 +18,8 @@ snapshot) {
  if (snapshot.connectionState == ConnectionState.done)
 {
  // TODO: Add RecipesGridView Here
+ return RecipesGridView(recipes: snapshot.data ?? []);
  // 5
- return const Center(child: Text('Recipes Screen'));
  } else {
  // 6
  return const Center(child:
